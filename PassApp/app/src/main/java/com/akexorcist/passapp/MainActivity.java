94caf2a9f68e87d9628293b80beb9b143f4769d5
Akexorcist <akexorcist@gmail.com>
2016-03-31 10:04:35 +0700

    Add fingerprint authentication sample code

---
+++ PassApp/app/src/main/java/com/akexorcist/passapp/MainActivity.java
@@ -0,0 +1,123 @@
+package com.akexorcist.passapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.samsung.android.sdk.SsdkUnsupportedException;
+import com.samsung.android.sdk.pass.Spass;
+import com.samsung.android.sdk.pass.SpassFingerprint;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SpassFingerprint mSpassFingerprint;
+    private Spass mSpass;
+
+    private Button btnVerify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnVerify = (Button) findViewById(R.id.btn_verify);
+        btnVerify.setOnClickListener(this);
+
+        if (setupSPass()) {
+            btnVerify.setEnabled(true);
+        } else {
+            btnVerify.setEnabled(false);
+        }
+    }
+
+    private boolean setupSPass() {
+        mSpass = new Spass();
+        try {
+            mSpass.initialize(this);
+            if (mSpass.isFeatureEnabled(Spass.DEVICE_FINGERPRINT)) {
+                mSpassFingerprint = new SpassFingerprint(this);
+                return true;
+            }
+        } catch (SsdkUnsupportedException e) {
+            // Fingerprint Service is not supported in this SDK
+        } catch (UnsupportedOperationException e) {
+            // Fingerprint Service is not supported in this device
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btnVerify) {
+            startIdentifyDialog(true);
+        }
+    }
+
+    private void startIdentifyDialog(boolean backup) {
+        if (mSpassFingerprint != null) {
+            mSpassFingerprint.startIdentifyWithDialog(this, mIdentifyListenerDialog, backup);
+        }
+    }
+
+    private SpassFingerprint.IdentifyListener mIdentifyListenerDialog = new SpassFingerprint.IdentifyListener() {
+        @Override
+        public void onFinished(int eventStatus) {
+            Toast.makeText(MainActivity.this, getEventStatusName(eventStatus), Toast.LENGTH_SHORT).show();
+
+            if (eventStatus == SpassFingerprint.STATUS_AUTHENTIFICATION_SUCCESS) {
+                // Authentification Success
+                int index = mSpassFingerprint.getIdentifiedFingerprintIndex();
+                Toast.makeText(MainActivity.this, "Fingerprint Index : " + index, Toast.LENGTH_SHORT).show();
+            } else if (eventStatus == SpassFingerprint.STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS) {
+                // Authentification Password Success
+            } else if (eventStatus == SpassFingerprint.STATUS_USER_CANCELLED
+                    || eventStatus == SpassFingerprint.STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE) {
+                // Authentification Cancel
+            } else if (eventStatus == SpassFingerprint.STATUS_TIMEOUT_FAILED) {
+                // Authentification Timeout
+            } else if (eventStatus == SpassFingerprint.STATUS_QUALITY_FAILED) {
+                // Authentification Quality Failed
+            } else {
+                // Authentification Failed
+            }
+        }
+
+        @Override
+        public void onReady() {
+            // Fingerprint sensor is ready
+        }
+
+        @Override
+        public void onStarted() {
+            // User touched fingerprint sensor
+        }
+
+        @Override
+        public void onCompleted() {
+            // Identify is completed
+        }
+    };
+
+    private static String getEventStatusName(int eventStatus) {
+        switch (eventStatus) {
+            case SpassFingerprint.STATUS_AUTHENTIFICATION_SUCCESS:
+                return "STATUS_AUTHENTIFICATION_SUCCESS";
+            case SpassFingerprint.STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS:
+                return "STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS";
+            case SpassFingerprint.STATUS_TIMEOUT_FAILED:
+                return "STATUS_TIMEOUT";
+            case SpassFingerprint.STATUS_SENSOR_FAILED:
+                return "STATUS_SENSOR_ERROR";
+            case SpassFingerprint.STATUS_USER_CANCELLED:
+                return "STATUS_USER_CANCELLED";
+            case SpassFingerprint.STATUS_QUALITY_FAILED:
+                return "STATUS_QUALITY_FAILED";
+            case SpassFingerprint.STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE:
+                return "STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE";
+            default:
+                return "STATUS_AUTHENTIFICATION_FAILED";
+        }
+    }
+}

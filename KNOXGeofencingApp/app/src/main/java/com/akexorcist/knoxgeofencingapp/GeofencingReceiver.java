d5984c8d5667878c6a8926772cdcce31fb523f31
Akexorcist <akexorcist@gmail.com>
2016-05-10 10:05:33 +0700

    Change GeofenceReceiver to GeofencingRecever Remove log code Move <receiver> to actual position

--- KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/GeofencingReceiver.java
+++ KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/GeofencingReceiver.java
@@ -4,23 +4,20 @@ import android.app.enterprise.geofencing.Geofencing;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 /**
  * Created by Akexorcist on 5/10/16 AD.
  */
-public class GeofenceReceiver extends BroadcastReceiver {
+public class GeofencingReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_INSIDE_GEOFENCE)) {
-            Log.e("Check", "Inside Geofencing");
+            // Do something when device inside geofence
         } else if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_OUTSIDE_GEOFENCE)) {
-            Log.e("Check", "Outside Geofencing");
+            // Do something when device outside geofence
         } else if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_LOCATION_UNAVAILABLE)) {
-            Log.e("Check", "Location Unavailable");
-        } else {
-            Log.e("Check", "Receive Something");
+            // Do something when device location unavailable
         }
     }
 }

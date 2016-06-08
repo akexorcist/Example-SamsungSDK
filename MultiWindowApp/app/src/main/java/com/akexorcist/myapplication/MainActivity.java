ffee79c8374690fe60e1407851808b553f20e329
Akexorcist <akexorcist@gmail.com>
2016-06-09 00:28:54 +0700

    Add another code about simple animation

--- MultiWindowApp/app/src/main/java/com/akexorcist/myapplication/MainActivity.java
+++ MultiWindowApp/app/src/main/java/com/akexorcist/myapplication/MainActivity.java
@@ -1,15 +1,46 @@
 package com.akexorcist.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    private ImageView ivAndroidLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ivAndroidLogo = (ImageView) findViewById(R.id.iv_android_logo);
+        ivAndroidLogo.setOnTouchListener(this);
 
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_DOWN) {
+            scaleDown();
+        } else if (action == MotionEvent.ACTION_UP) {
+            scaleDefault();
+        }
+        return true;
+    }
+
+    private void scaleDown() {
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.animator_scale_down);
+        animator.setTarget(ivAndroidLogo);
+        animator.start();
+    }
+
+    private void scaleDefault() {
+        Animator animator = AnimatorInflater.loadAnimator(this, R.animator.animator_scale_default);
+        animator.setTarget(ivAndroidLogo);
+        animator.start();
     }
 }

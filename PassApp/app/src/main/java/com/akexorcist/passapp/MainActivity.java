49745b79faac1752d39858def13264ccd310093f
Akexorcist <akexorcist@gmail.com>
2016-06-08 07:57:21 +0700

    Custom layout and add text description Remove important code for Spass and place todo instead

--- PassApp/app/src/main/java/com/akexorcist/passapp/MainActivity.java
+++ PassApp/app/src/main/java/com/akexorcist/passapp/MainActivity.java
@@ -5,16 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
-import com.samsung.android.sdk.SsdkUnsupportedException;
-import com.samsung.android.sdk.pass.Spass;
 import com.samsung.android.sdk.pass.SpassFingerprint;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Spass spass;
-    private SpassFingerprint spassFingerprint;
+    // TODO Declare Spass and SpassFingerprint instance
 
+    private TextView tvLoginDescription;
     private Button btnVerify;
 
     @Override
@@ -22,28 +20,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bindView();
+        setup();
+    }
+
+    public void bindView() {
+        tvLoginDescription = (TextView) findViewById(R.id.tv_login_description);
         btnVerify = (Button) findViewById(R.id.btn_verify);
+    }
+
+    public void setup() {
         btnVerify.setOnClickListener(this);
 
         if (setupSPass()) {
             btnVerify.setEnabled(true);
+            tvLoginDescription.setText(R.string.sign_in_description);
         } else {
             btnVerify.setEnabled(false);
-            Toast.makeText(this, "Device doesn't support Pass SDK.", Toast.LENGTH_SHORT).show();
+            tvLoginDescription.setText(R.string.device_does_not_supported_pass_sdk);
         }
     }
 
     private boolean setupSPass() {
-        spass = new Spass();
-        try {
-            spass.initialize(this);
-            if (spass.isFeatureEnabled(Spass.DEVICE_FINGERPRINT)) {
-                spassFingerprint = new SpassFingerprint(this);
-                return true;
-            }
-        } catch (SsdkUnsupportedException | UnsupportedOperationException e) {
-            // Fingerprint Service is not supported in this SDK
-        }
+        // TODO Initialize Pass SDK and return boolean result
+
         return false;
     }
 
@@ -55,32 +55,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void startIdentifyDialog(boolean backup) {
-        if (spassFingerprint != null) {
-            spassFingerprint.startIdentifyWithDialog(this, mIdentifyListenerDialog, backup);
-        }
+        // TODO Start fingerprint scanning by with default identify dialog
     }
 
     private SpassFingerprint.IdentifyListener mIdentifyListenerDialog = new SpassFingerprint.IdentifyListener() {
         @Override
         public void onFinished(int eventStatus) {
-            Toast.makeText(MainActivity.this, getEventStatusName(eventStatus), Toast.LENGTH_SHORT).show();
-            if (eventStatus == SpassFingerprint.STATUS_AUTHENTIFICATION_SUCCESS) {
-                // Authentication Success
-                int index = spassFingerprint.getIdentifiedFingerprintIndex();
-                Toast.makeText(MainActivity.this, "Fingerprint Index : " + index, Toast.LENGTH_SHORT).show();
-                openInfoActivity();
-            } else if (eventStatus == SpassFingerprint.STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS) {
-                // Authentication Password Success
-            } else if (eventStatus == SpassFingerprint.STATUS_USER_CANCELLED
-                    || eventStatus == SpassFingerprint.STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE) {
-                // Authentication Cancel
-            } else if (eventStatus == SpassFingerprint.STATUS_TIMEOUT_FAILED) {
-                // Authentication Timeout
-            } else if (eventStatus == SpassFingerprint.STATUS_QUALITY_FAILED) {
-                // Authentication Quality Failed
-            } else {
-                // Authentication Failed
-            }
+            // Fingerprint scanning is finish
+
+            // TODO Check result from fingerprint scanning
+            // if status is success, open next activity with openInfoActivity();
         }
 
         @Override
@@ -98,27 +82,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             // Identify is completed
         }
     };
-
-    private String getEventStatusName(int eventStatus) {
-        switch (eventStatus) {
-            case SpassFingerprint.STATUS_AUTHENTIFICATION_SUCCESS:
-                return "STATUS_AUTHENTIFICATION_SUCCESS";
-            case SpassFingerprint.STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS:
-                return "STATUS_AUTHENTIFICATION_PASSWORD_SUCCESS";
-            case SpassFingerprint.STATUS_TIMEOUT_FAILED:
-                return "STATUS_TIMEOUT";
-            case SpassFingerprint.STATUS_SENSOR_FAILED:
-                return "STATUS_SENSOR_ERROR";
-            case SpassFingerprint.STATUS_USER_CANCELLED:
-                return "STATUS_USER_CANCELLED";
-            case SpassFingerprint.STATUS_QUALITY_FAILED:
-                return "STATUS_QUALITY_FAILED";
-            case SpassFingerprint.STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE:
-                return "STATUS_USER_CANCELLED_BY_TOUCH_OUTSIDE";
-            default:
-                return "STATUS_AUTHENTIFICATION_FAILED";
-        }
-    }
 
     private void openInfoActivity() {
         startActivity(new Intent(this, InfoActivity.class));

0d5a68490ceb2e314a4ee0fc970ee1528b08e250
Akexorcist <akexorcist@gmail.com>
2016-06-10 08:17:17 +0700

    Remove unused resource

--- KNOXMultiUserApp/app/src/main/java/com/akexorcist/knoxmultiuserapp/DoSomethingActivity.java
+++ KNOXMultiUserApp/app/src/main/java/com/akexorcist/knoxmultiuserapp/DoSomethingActivity.java
@@ -3,15 +3,8 @@ package com.akexorcist.knoxmultiuserapp;
 import android.app.enterprise.multiuser.MultiUserManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-public class DoSomethingActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btnGeofencingStart;
-    private Button btnGeofencingStop;
-    private Button btnCreateGeofence;
-    private Button btnClearGeofence;
-
+public class DoSomethingActivity extends AppCompatActivity{
     private MultiUserManager multiUserManager;
 
     @Override
@@ -25,37 +18,17 @@ public class DoSomethingActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void bindView() {
-        btnGeofencingStart = (Button) findViewById(R.id.btn_geofencing_start);
-        btnGeofencingStop = (Button) findViewById(R.id.btn_geofencing_stop);
-        btnCreateGeofence = (Button) findViewById(R.id.btn_create_geofence);
-        btnClearGeofence = (Button) findViewById(R.id.btn_clear_geofence);
+
     }
 
     private void setupView() {
-        btnGeofencingStart.setOnClickListener(this);
-        btnGeofencingStop.setOnClickListener(this);
-        btnCreateGeofence.setOnClickListener(this);
-        btnClearGeofence.setOnClickListener(this);
+
     }
 
     private void setupThing() {
         MultiUserManager multiUserManager = MultiUserManager.getInstance(this);
-        multiUserManager.multipleUsersAllowed();
         if (multiUserManager.multipleUsersSupported()) {
-            // Call MultiUserManager method
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v == btnGeofencingStart) {
-//            startGeofencing();
-        } else if (v == btnGeofencingStop) {
-//            stopGeofencing();
-        } else if (v == btnCreateGeofence) {
-//            createGeofence();
-        } else if (v == btnClearGeofence) {
-//            clearGeofence();
+            // Call MultipleUserManager method
         }
     }
 }

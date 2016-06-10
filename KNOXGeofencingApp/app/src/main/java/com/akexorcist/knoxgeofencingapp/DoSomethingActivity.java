52d97cabd02050295bab6a0275e7e009642c3fcd
Akexorcist <akexorcist@gmail.com>
2016-06-10 07:00:58 +0700

    Change code to practice code

--- KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/DoSomethingActivity.java
+++ KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/DoSomethingActivity.java
@@ -1,22 +1,9 @@
 package com.akexorcist.knoxgeofencingapp;
 
-import android.app.enterprise.geofencing.CircularGeofence;
-import android.app.enterprise.geofencing.Geofence;
-import android.app.enterprise.geofencing.Geofencing;
-import android.app.enterprise.geofencing.LatLongPoint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import com.akexorcist.knoxgeofencingapp.bus.BusProvider;
-import com.akexorcist.knoxgeofencingapp.bus.event.InsideGeofenceEvent;
-import com.akexorcist.knoxgeofencingapp.bus.event.LocationUnavailableEvent;
-import com.akexorcist.knoxgeofencingapp.bus.event.OutsideGeofenceEvent;
-import com.squareup.otto.Subscribe;
-
-import java.util.List;
 
 public class DoSomethingActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btnGeofencingStart;
@@ -24,7 +11,7 @@ public class DoSomethingActivity extends AppCompatActivity implements View.OnCli
     private Button btnCreateGeofence;
     private Button btnClearGeofence;
 
-    private Geofencing geofencingService;
+    // TODO Declare geofencing service
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +21,6 @@ public class DoSomethingActivity extends AppCompatActivity implements View.OnCli
         bindView();
         setupView();
         setupThing();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        BusProvider.getProvider().unregister(this);
     }
 
     private void bindView() {
@@ -57,14 +38,15 @@ public class DoSomethingActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void setupThing() {
-        BusProvider.getProvider().register(this);
-        geofencingService = Geofencing.getInstance(this);
+        // TODO Create geofencing service instance
+
     }
 
     @Override
     public void onClick(View v) {
         if (v == btnGeofencingStart) {
             startGeofencing();
+            createGeofence();
         } else if (v == btnGeofencingStop) {
             stopGeofencing();
         } else if (v == btnCreateGeofence) {
@@ -75,46 +57,22 @@ public class DoSomethingActivity extends AppCompatActivity implements View.OnCli
     }
 
     private void createGeofence() {
-        LatLongPoint center = new LatLongPoint(13.678505, 100.615474);
-        double radius = 1000;
-        CircularGeofence circularGeofence = new CircularGeofence(center, radius);
-        int id = geofencingService.createGeofence(circularGeofence);
-        if (id == -1) {
-            Toast.makeText(this, R.string.already_exists_geofence, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, getString(R.string.geofence_was_created, id), Toast.LENGTH_SHORT).show();
-        }
+        // TODO Create geofence, then add into geofencing service
+
     }
 
     private void clearGeofence() {
-        List<Geofence> geofenceList = geofencingService.getGeofences();
-        if (geofenceList != null) {
-            for (Geofence geofence : geofenceList) {
-                geofencingService.destroyGeofence(geofence.id);
-            }
-        }
+        // TODO Clear all geofence in geofencing service
+
     }
 
     private void startGeofencing() {
-        geofencingService.startGeofencing();
+        // TODO Start geofencing
+
     }
 
     private void stopGeofencing() {
-        geofencingService.stopGeofencing();
-    }
+        // TODO Stop geofencing
 
-    @Subscribe
-    public void onInsideGeofenceEvent(InsideGeofenceEvent event) {
-        // When device is inside geofence
-    }
-
-    @Subscribe
-    public void onOutsideGeofenceEvent(OutsideGeofenceEvent event) {
-        // When device is outside geofence
-    }
-
-    @Subscribe
-    public void onLocationUnavailable(LocationUnavailableEvent event) {
-        // When device location is unavailable
     }
 }
--- KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/GeofencingReceiver.java
+++ KNOXGeofencingApp/app/src/main/java/com/akexorcist/knoxgeofencingapp/GeofencingReceiver.java
@@ -6,11 +6,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import com.akexorcist.knoxgeofencingapp.bus.BusProvider;
-import com.akexorcist.knoxgeofencingapp.bus.event.InsideGeofenceEvent;
-import com.akexorcist.knoxgeofencingapp.bus.event.LocationUnavailableEvent;
-import com.akexorcist.knoxgeofencingapp.bus.event.OutsideGeofenceEvent;
-
 /**
  * Created by Akexorcist on 5/10/16 AD.
  */
@@ -20,13 +15,11 @@ public class GeofencingReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_INSIDE_GEOFENCE)) {
-            int[] geofenceIdList = intent.getIntArrayExtra(Geofencing.INTENT_EXTRA_ID);
-            BusProvider.getProvider().post(new InsideGeofenceEvent(geofenceIdList));
+            // TODO Do something when device inside geofence
         } else if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_OUTSIDE_GEOFENCE)) {
-            int[] geofenceIdList = intent.getIntArrayExtra(Geofencing.INTENT_EXTRA_ID);
-            BusProvider.getProvider().post(new OutsideGeofenceEvent(geofenceIdList));
+            // TODO Do something when device outside geofence
         } else if (action.equalsIgnoreCase(Geofencing.ACTION_DEVICE_LOCATION_UNAVAILABLE)) {
-            BusProvider.getProvider().post(new LocationUnavailableEvent());
+            // TODO Do something when device location unavailable
         }
     }
 
